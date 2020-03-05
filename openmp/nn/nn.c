4c0fa79e0b6c2a07671906fee285bb0f6e9ab7ac
Zhengrong Wang <seanyukigeek@gmail.com>
2020-03-04 21:39:05 -0800

    [misc] Make nn work.

--- openmp/nn/nn.c
+++ openmp/nn/nn.c
@@ -1,9 +1,14 @@
 #include <math.h>
 #include <omp.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+
+#ifdef GEM_FORGE
+#include "gem5/m5ops.h"
+#endif
 
 #define MAX_ARGS 10
 #define REC_LENGTH 49     // size of a record in db
@@ -12,10 +17,11 @@
 #define OPEN 10000        // initial value of nearest neighbors
 struct Neighbor {
   char entry[REC_LENGTH];
+  uint32_t update_idx;
   double dist;
 };
 
-__attribute__((packed)) struct Record {
+struct __attribute__((packed)) Record {
   int year;
   int month;
   int date;
@@ -54,8 +60,49 @@ struct Record sandbox[REC_WINDOW];
 char sandbox[REC_LENGTH * REC_WINDOW];
 #endif
 
-void process(FILE *flist, struct Neighbor *neighbors, int k, float target_lat,
-             float target_long) {
+void final_reduce(int k, int threads, struct Neighbor *neighbors,
+                  struct Neighbor *reduce_neighbors) {
+  for (int i = 0; i < threads * k; i++) {
+    float dist = reduce_neighbors[i].dist;
+    uint32_t update_idx = reduce_neighbors[i].update_idx;
+    reduce_neighbors[i].update_idx = UINT32_MAX;
+    if (update_idx != UINT32_MAX) {
+      float max_dist = -1;
+      int max_idx = 0;
+      // find a neighbor with greatest dist and take his spot if allowed!
+      for (int j = 0; j < k; j++) {
+        double dist = neighbors[j].dist;
+        if (dist > max_dist) {
+          max_dist = dist;
+          max_idx = j;
+        }
+      }
+      // compare each record with max value to find the nearest neighbor
+      if (dist < max_dist) {
+        neighbors[max_idx].dist = dist;
+        neighbors[max_idx].update_idx = update_idx;
+      }
+    }
+  }
+
+  // Set the final value.
+  for (int i = 0; i < k; ++i) {
+    uint32_t update_idx = neighbors[i].update_idx;
+    neighbors[i].update_idx = UINT32_MAX;
+    if (update_idx != UINT32_MAX) {
+#if IS_BINARY == 1
+      strcpy(neighbors[i].entry, sandbox[update_idx].name);
+#else
+      sandbox[(update_idx + 1) * REC_LENGTH - 1] = '\0';
+      strcpy(neighbors[i].entry, sandbox + update_idx * REC_LENGTH);
+#endif
+    }
+  }
+}
+
+void process(FILE *flist, struct Neighbor *neighbors,
+             struct Neighbor *reduce_neighbors, int k, int threads,
+             float target_lat, float target_long) {
 
   char dbname[64];
   if (fscanf(flist, "%s\n", dbname) != 1) {
@@ -71,13 +118,41 @@ void process(FILE *flist, struct Neighbor *neighbors, int k, float target_lat,
 
   float *z = (float *)malloc(REC_WINDOW * sizeof(float));
 
+#ifdef GEM_FORGE
+  m5_detail_sim_start();
+#ifdef GEM_FORGE_WARM_CACHE
+#define VOLATILE_LOAD(x, i) volatile uint8_t x##V = ((uint8_t *)x)[i];
+  for (uint64_t i = 0; i < k * sizeof(struct Neighbor); i += 64) {
+    VOLATILE_LOAD(neighbors, i);
+  }
+  for (uint64_t i = 0; i < threads * k * sizeof(struct Neighbor); i += 64) {
+    VOLATILE_LOAD(reduce_neighbors, i);
+  }
+  for (uint64_t i = 0; i < REC_WINDOW * sizeof(float); i += 64) {
+    VOLATILE_LOAD(z, i);
+  }
+// Make sure we start the threads before entering ROI.
+#pragma omp parallel for schedule(static)
+  for (uint64_t i = 0; i < threads; ++i) {
+    VOLATILE_LOAD(reduce_neighbors, i);
+  }
+#undef VOLATILE_LOAD
+  m5_reset_stats(0, 0);
+#endif
+#endif
+
   int done = 0;
   while (!done) {
+
+#ifdef GEM_FORGE
+    m5_work_begin(0, 0);
+#endif
+
 // Read in REC_WINDOW number of records
 #if IS_BINARY == 1
-    size_t rec_count = fread(sandbox, sizeof(struct Record), REC_WINDOW, fp);
+    const int rec_count = fread(sandbox, sizeof(struct Record), REC_WINDOW, fp);
 #else
-    size_t rec_count = fread(sandbox, REC_LENGTH, REC_WINDOW, fp);
+    const int rec_count = fread(sandbox, REC_LENGTH, REC_WINDOW, fp);
 #endif
     if (rec_count != REC_WINDOW) {
       if (!ferror(flist)) { // an eof occured
@@ -104,7 +179,13 @@ void process(FILE *flist, struct Neighbor *neighbors, int k, float target_lat,
       }
     }
 
-#pragma omp parallel for shared(z, target_lat, target_long) schedule(static)
+#ifdef GEM_FORGE
+    m5_work_end(0, 0);
+    m5_work_begin(1, 0);
+#endif
+
+#pragma omp parallel for firstprivate(z, target_lat, target_long, rec_count)   \
+    schedule(static)
     for (int i = 0; i < rec_count; i++) {
 #if IS_BINARY == 1
       float tmp_lat = sandbox[i].lat;
@@ -118,28 +199,50 @@ void process(FILE *flist, struct Neighbor *neighbors, int k, float target_lat,
                   ((tmp_long - target_long) * (tmp_long - target_long)));
     }
 
+#ifdef GEM_FORGE
+    m5_work_end(1, 0);
+    m5_work_begin(2, 0);
+#endif
+
+#pragma omp parallel for firstprivate(rec_count, z, reduce_neighbors)          \
+    schedule(static)
     for (int i = 0; i < rec_count; i++) {
+      int tid = omp_get_thread_num();
       float max_dist = -1;
       int max_idx = 0;
       // find a neighbor with greatest dist and take his spot if allowed!
-      for (int j = 0; j < k; j++) {
-        if (neighbors[j].dist > max_dist) {
-          max_dist = neighbors[j].dist;
+      int reduce_start = tid * k;
+      for (int j = reduce_start; j < reduce_start + k; j++) {
+        double dist = reduce_neighbors[j].dist;
+        if (dist > max_dist) {
+          max_dist = dist;
           max_idx = j;
         }
       }
       // compare each record with max value to find the nearest neighbor
-      if (z[i] < neighbors[max_idx].dist) {
-#if IS_BINARY == 1
-        strcpy(neighbors[max_idx].entry, sandbox[i].name);
-#else
-        sandbox[(i + 1) * REC_LENGTH - 1] = '\0';
-        strcpy(neighbors[max_idx].entry, sandbox + i * REC_LENGTH);
-#endif
-        neighbors[max_idx].dist = z[i];
+      float dist = z[i];
+      if (dist < max_dist) {
+        reduce_neighbors[max_idx].dist = dist;
+        reduce_neighbors[max_idx].update_idx = i;
       }
     }
+
+#ifdef GEM_FORGE
+    m5_work_end(2, 0);
+    m5_work_begin(3, 0);
+#endif
+
+    // Final reduce.
+    final_reduce(k, threads, neighbors, reduce_neighbors);
+#ifdef GEM_FORGE
+    m5_work_end(3, 0);
+#endif
   } // End while loop
+
+#ifdef GEM_FORGE
+  m5_detail_sim_end();
+  exit(0);
+#endif
 }
 
 int main(int argc, char *argv[]) {
@@ -163,17 +266,26 @@ int main(int argc, char *argv[]) {
   omp_set_num_threads(threads);
 
   struct Neighbor *neighbors = malloc(k * sizeof(struct Neighbor));
-  if (neighbors == NULL) {
+  struct Neighbor *reduce_neighbors =
+      malloc(threads * k * sizeof(struct Neighbor));
+  if (neighbors == NULL || reduce_neighbors == NULL) {
     fprintf(stderr, "no room for neighbors\n");
     exit(1);
   }
   for (int j = 0; j < k; j++) {
     // Initialize list of nearest neighbors to very large dist
     neighbors[j].dist = OPEN;
+    neighbors[j].update_idx = UINT32_MAX;
+  }
+  for (int j = 0; j < k * threads; j++) {
+    // Initialize list of nearest neighbors to very large dist
+    reduce_neighbors[j].dist = OPEN;
+    reduce_neighbors[j].update_idx = UINT32_MAX;
   }
 
   /**** main processing ****/
-  process(flist, neighbors, k, target_lat, target_long);
+  process(flist, neighbors, reduce_neighbors, k, threads, target_lat,
+          target_long);
 
   fprintf(stderr, "The %d nearest neighbors are:\n", k);
   for (int j = 0; j < k; j++) {
