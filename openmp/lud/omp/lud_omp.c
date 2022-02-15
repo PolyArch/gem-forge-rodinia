b2e868bb422a528c275971f69547eba71c00888a
Zhengrong Wang <seanyukigeek@gmail.com>
2022-02-15 17:51:20 -0600

    get rid of the temp buffer in lud

--- openmp/lud/omp/lud_omp.c
+++ openmp/lud/omp/lud_omp.c
@@ -5,8 +5,8 @@ extern int omp_num_threads;
 
 #define BS 16
 
-#define AA(_i, _j) a[offset * size + _i * size + _j + offset]
-#define BB(_i, _j) a[_i * size + _j]
+#define AA(_i, _j) a[offset * size + (_i)*size + (_j) + offset]
+#define BB(_i, _j) a[(_i)*size + (_j)]
 
 void lud_diagonal_omp(float *a, int size, int offset) {
   int i, j, k;
@@ -46,15 +46,7 @@ void lud_omp(float *a, int size) {
 #pragma omp parallel for default(none)                                         \
     shared(size, chunks_per_inter, chunks_in_inter_row, offset, a)
     for (int chunk_idx = 0; chunk_idx < chunks_in_inter_row; chunk_idx++) {
-      float temp[BS * BS] __attribute__((aligned(64)));
 
-      // copy the diagonal block.
-      for (int i = 0; i < BS; i++) {
-#pragma omp simd
-        for (int j = 0; j < BS; j++) {
-          temp[i * BS + j] = a[size * (i + offset) + offset + j];
-        }
-      }
       int i_global = offset;
       int j_global = offset;
 
@@ -64,11 +56,14 @@ void lud_omp(float *a, int size) {
         for (int i = 0; i < BS; i++) {
           float sum = 0.f;
           for (int k = 0; k < i; k++) {
-            sum += temp[BS * i + k] * BB((i_global + k), (j_global + j));
+            float diag = BB(offset + i, offset + k);
+            float here = BB(i_global + k, j_global + j);
+            sum += diag * here;
           }
           int i_here = i_global + i;
           int j_here = j_global + j;
-          BB(i_here, j_here) = BB(i_here, j_here) - sum;
+          float here = BB(i_here, j_here);
+          BB(i_here, j_here) = here - sum;
         }
       }
 
@@ -80,12 +75,14 @@ void lud_omp(float *a, int size) {
         for (int j = 0; j < BS; j++) {
           float sum = 0.f;
           for (int k = 0; k < j; k++) {
-            sum += BB((i_global + i), (j_global + k)) * temp[BS * k + j];
+            float diag = BB(offset + k, offset + j);
+            float here = BB(i_global + i, j_global + k);
+            sum += diag * here;
           }
           int i_here = i_global + i;
           int j_here = j_global + j;
-          a[size * i_here + j_here] = (a[size * i_here + j_here] - sum) /
-                                      a[size * (offset + j) + offset + j];
+          float here = BB(i_here, j_here);
+          BB(i_here, j_here) = here / BB(offset + j, offset + j);
         }
       }
     }
@@ -96,28 +93,19 @@ void lud_omp(float *a, int size) {
 #pragma omp parallel for schedule(auto) default(none)                          \
     shared(size, chunks_per_inter, chunks_in_inter_row, offset, a)
     for (int chunk_idx = 0; chunk_idx < chunks_per_inter; chunk_idx++) {
-      float temp_top[BS * BS] __attribute__((aligned(64)));
-      float temp_left[BS * BS] __attribute__((aligned(64)));
       float sum[BS] __attribute__((aligned(64))) = {0.f};
 
       int i_global = offset + BS * (1 + chunk_idx / chunks_in_inter_row);
       int j_global = offset + BS * (1 + chunk_idx % chunks_in_inter_row);
-
-      // Load the top perimeter and left perimeter block.
-      for (int i = 0; i < BS; i++) {
-#pragma omp simd
-        for (int j = 0; j < BS; j++) {
-          temp_top[i * BS + j] = a[size * (i + offset) + j + j_global];
-          temp_left[i * BS + j] = a[size * (i + i_global) + offset + j];
-        }
-      }
 
       // Basically Block[][] -= Left[][] * Top[][].
       for (int i = 0; i < BS; i++) {
         for (int k = 0; k < BS; k++) {
 #pragma omp simd
           for (int j = 0; j < BS; j++) {
-            sum[j] += temp_left[BS * i + k] * temp_top[BS * k + j];
+            float left = BB(i_global + i, offset + k);
+            float top = BB(offset + k, j_global + j);
+            sum[j] += left * top;
           }
         }
 #pragma omp simd
